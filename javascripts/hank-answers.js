72d174c3882b810e3fc6966245ffcc968e57ce01
Steven Nugent <stevennugentdev@gmail.com>
2013-12-19 20:16:16 -0800

    Fixed error when selecting random response

--- javascripts/hank-answers.js
+++ javascripts/hank-answers.js
@@ -1,6 +1,6 @@
 function hankAnswers(inputField) {
 	var hankSays = document.getElementById("hank-says")
-	var possibleAnswers = ["I'm not sure, but give me a treat!",
+	gvar possibleAnswers = ["I'm not sure, but give me a treat!",
 						   "How should I know? I'm just a dog. A talking dog, but still...",
 						   "Yes! I mean, No! ...maybe? Woof.",
 						   "Let's think about this. I would...ANOTHER DOG! WOOF! WOOF!"]
