83a754f0a9018d5c11dcec6e132c369728bbc663
Steven Nugent <stevennugentdev@gmail.com>
2013-12-19 20:15:10 -0800

    Fix error when selecing random response

--- javascripts/hank-answers.js
+++ javascripts/hank-answers.js
@@ -4,7 +4,7 @@ function hankAnswers(inputField) {
 						   "How should I know? I'm just a dog. A talking dog, but still...",
 						   "Yes! I mean, No! ...maybe? Woof.",
 						   "Let's think about this. I would...ANOTHER DOG! WOOF! WOOF!"]
-	var randomNumber = Math.floor(Math.random()*possibleAnswers.length) + 1
+	var randomNumber = Math.floor(Math.random()*possibleAnswers.length)
 	var randomResponse = possibleAnswers[randomNumber]
 	hankSays.innerText = randomResponse
 	inputField.value = ""
